f98e1375a371d3e8e8d9b12f4f9cebbf6c39e04e
Kay Sharrock <sharrock.kay@gmail.com>
2015-12-07 23:40:29 +0000

    Added tests dealing with navigation on quiz pages.
    Tests for:
    - The first quiz in a topic contains a link to the next quiz
    - The first quiz in a topic contains a link to the video before
    - The second quiz contains a link to the previous quiz
    - The final quiz contains a link to the first video of the next topic

--- spec/features/quiz_spec.rb
+++ spec/features/quiz_spec.rb
@@ -19,7 +19,7 @@ feature "A quiz page" do
     end
   end
   
-  it "has quesiton 1's answers" do
+  it "has question 1's answers" do
     within('#main form') do
       expect(page).to have_content 'Static'
       expect(page).to have_content 'Dynamic'
@@ -37,6 +37,34 @@ feature "A quiz page" do
   it "contains a submit button" do
     within('#main form') do
       expect(page).to have_selector(:css, "button[type=\"submit\"]")
+    end
+  end
+
+  context "navigation" do
+    context "The first quiz in a topic with multiple quizzes" do
+      it "contains a link to the next quiz" do
+        visit('/phases/fundamentals/ruby-quiz')
+        expect(page).to have_link 'Ruby Quiz 2', href: "/phases/fundamentals/ruby-quiz-2"
+      end
+
+      it "contains a link to the last video of the topic" do
+        visit('/phases/fundamentals/ruby-quiz')
+        expect(page).to have_link 'Ruby Gems', href: "/phases/fundamentals/ruby-gems"
+      end
+    end
+
+    context "The second quiz in a topic" do
+      it "contains a link to the previous quiz" do
+        visit('/phases/fundamentals/ruby-quiz-2')
+        expect(page).to have_link 'Ruby Quiz', href: "/phases/fundamentals/ruby-quiz"
+      end
+    end
+
+    context "The final quiz in a topic" do
+      it "contains a link to the first video of the next topic" do
+        visit('/phases/fundamentals/ruby-quiz-2')
+        expect(page).to have_link 'Planning vs. reacting', href: "/phases/fundamentals/planning-vs-reacting"
+      end
     end
   end
 end

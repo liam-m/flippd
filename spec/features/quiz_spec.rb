70b16b1afd286d63fb7687c5f3b541ea33405c81
liam-m <liam-m@users.noreply.github.com>
2015-12-08 22:41:30 +0000

    Put quiz display tests in a context

--- spec/features/quiz_spec.rb
+++ spec/features/quiz_spec.rb
@@ -1,42 +1,44 @@
 feature "A quiz page" do
-  before(:each) { visit('/phases/fundamentals/ruby-quiz') }
+  context "quiz display" do
+    before(:each) { visit('/phases/fundamentals/ruby-quiz') }
 
-  it "contains the quiz's title" do
-    within('#main h1') do
-      expect(page).to have_content 'Ruby Quiz'
+    it "contains the quiz's title" do
+      within('#main h1') do
+        expect(page).to have_content 'Ruby Quiz'
+      end
     end
-  end
 
-  it "contains question 1" do
-    within('#main form') do
-      expect(page).to have_content 'Is Ruby a static or dynamic language?'
+    it "contains question 1" do
+      within('#main form') do
+        expect(page).to have_content 'Is Ruby a static or dynamic language?'
+      end
     end
-  end
-  
-  it "contains question 2" do
-    within('#main form') do
-      expect(page).to have_content 'What does TDD stand for?'
-    end
-  end
-  
-  it "has question 1's answers" do
-    within('#main form') do
-      expect(page).to have_content 'Static'
-      expect(page).to have_content 'Dynamic'
-    end
-  end
-  
-  it "has question 2's answers" do
-    within('#main form') do
-      expect(page).to have_content 'Test Driven Development'
-      expect(page).to have_content 'Test Develop Drive'
-      expect(page).to have_content 'Till Development\'s Done'
-    end
-  end
 
-  it "contains a submit button" do
-    within('#main form') do
-      expect(page).to have_selector(:css, "button[type=\"submit\"]")
+    it "contains question 2" do
+      within('#main form') do
+        expect(page).to have_content 'What does TDD stand for?'
+      end
+    end
+
+    it "has question 1's answers" do
+      within('#main form') do
+        expect(page).to have_content 'Static'
+        expect(page).to have_content 'Dynamic'
+      end
+    end
+
+    it "has question 2's answers" do
+      within('#main form') do
+        expect(page).to have_content 'Test Driven Development'
+        expect(page).to have_content 'Test Develop Drive'
+        expect(page).to have_content 'Till Development\'s Done'
+      end
+    end
+
+    it "contains a submit button" do
+      within('#main form') do
+        expect(page).to have_selector(:css, "button[type=\"submit\"]")
+      end
     end
   end
 

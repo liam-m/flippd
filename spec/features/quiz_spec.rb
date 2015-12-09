fd9197d9e75ba2ba87aa1c26b6008c3447ac39a9
liam-m <liam-m@users.noreply.github.com>
2015-12-09 01:13:22 +0000

    Test for correct quiz submission

--- spec/features/quiz_spec.rb
+++ spec/features/quiz_spec.rb
@@ -42,6 +42,27 @@ feature "A quiz page" do
     end
   end
 
+  context "submission" do
+    before(:each) { visit('/phases/fundamentals/ruby-quiz') }
+
+    it "gives inline and overall feedback when the quiz is completed correctly" do
+      page.choose('Dynamic')
+      page.choose('Test Driven Development')
+      page.click_button('Submit')
+
+      within('#main form') do
+        # 2 ticks next to questions
+        expect(page).to have_xpath("//span[@class = 'glyphicon glyphicon-ok text-success']", :count => 2)
+
+        # 2 thumbs up next to answers
+        expect(page).to have_xpath("//span[@class = 'glyphicon glyphicon-thumbs-up']", :count => 2)
+
+        # Progress bar indicating 2/2 and success
+        expect(page).to have_xpath("//div[@class = 'progress-bar progress-bar-success']", :count => 1, :text => "2 / 2")
+      end
+    end
+  end
+
   context "navigation" do
     context "The first quiz in a topic with multiple quizzes" do
       it "contains a link to the next quiz" do

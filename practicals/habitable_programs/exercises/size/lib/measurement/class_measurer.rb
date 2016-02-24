fc21c4a5e5895f7a30dfebe58acea79beea0f989
liam-m <liam-m@users.noreply.github.com>
2016-02-24 17:01:04 +0000

    Implemented ClassMeasurer

--- practicals/habitable_programs/exercises/size/lib/measurement/class_measurer.rb
+++ practicals/habitable_programs/exercises/size/lib/measurement/class_measurer.rb
@@ -2,6 +2,29 @@ require_relative "../../../common/lib/measurement/measurer"
 require_relative "../../../common/lib/locator/class_locator"
 
 module Measurement
+  class ClassCounter < Parser::AST::Processor
+    attr_reader :n_methods, :n_class_methods, :n_attributes
+
+    def initialize
+      @n_methods, @n_class_methods, @n_attributes = 0, 0, 0
+    end
+
+    def on_def(node)
+      super(node)
+      @n_methods += 1
+    end
+
+    def on_defs(node)
+      super(node)
+      @n_class_methods += 1
+    end
+
+    def on_ivasgn(node)
+      super(node)
+      @n_attributes += 1
+    end
+  end
+
   class ClassMeasurer < Measurer
     def locator
       Locator::ClassLocator.new
@@ -17,19 +40,25 @@ module Measurement
     end
 
     def count_lines_of_code(clazz)
-      "?"
+      clazz.source.lines.to_a.size
     end
 
     def count_methods(clazz)
-      "?"
+      counter = ClassCounter.new
+      counter.process(clazz.ast)
+      counter.n_methods
     end
 
     def count_class_methods(clazz)
-      "?"
+      counter = ClassCounter.new
+      counter.process(clazz.ast)
+      counter.n_class_methods
     end
 
     def count_attributes(clazz)
-      "?"
+      counter = ClassCounter.new
+      counter.process(clazz.ast)
+      counter.n_attributes
     end
   end
 end

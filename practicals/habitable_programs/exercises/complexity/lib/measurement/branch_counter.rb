67bf2fdd0e62a0ab811f4528bf7ce614dc92c532
Nick Michael <nm852@york.ac.uk>
2016-03-01 14:00:42 +0000

    Branches ignore array indexing

--- practicals/habitable_programs/exercises/complexity/lib/measurement/branch_counter.rb
+++ practicals/habitable_programs/exercises/complexity/lib/measurement/branch_counter.rb
@@ -3,7 +3,7 @@ require_relative "counter"
 module Measurement
   class BranchCounter < Counter
     def on_send(node)
-        if not node.children[0].nil? and not [:==, :!=, :<=, :>=, :<, :>.include?(node.children[1])
+        if not node.children[0].nil? and not [:==, :!=, :<=, :>=, :<, :>, :[]].include?(node.children[1])
             increment
         end
 

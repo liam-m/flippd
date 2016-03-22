68bde7892d1ce3fd862317c11cfb33b58f0f476c
Kay Sharrock <sharrock.kay@gmail.com>
2016-03-22 13:31:28 +0000

    User XP and progress tracking now works except for badge awarding.

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,47 +14,54 @@ class User
   property :comments_left, Integer, :default => 0
   has n, :badges, :through => Resource
 
+
   ## An instance method to perform required calculations for leveling up.
   def update_level
-    self.update(:level_progress => (:level * 100 - :xp))
-    if :level_progress > 100
-      self.update(:level => (:level + 1))
+    new_level_progress = 100 - ((self['level'] * 100) - self['xp'])
+    self.update(:level_progress => new_level_progress)
+    if self['level_progress'] > 99
+      new_level = self['level'] + 1
+      self.update(:level => new_level)
       self.update_level
     end
   end
 
   ## Increments the number of quizzes completed, and checks for earned badges.
   def increment_quizzes
-    self.update(:quizzes_completed => (:quizzes_completed + 1))
-    if :quizzes_completed == 1
+    new_quizzes_completed = self['quizzes_completed'] + 1
+    self.update(:quizzes_completed => new_quizzes_completed)
+    if self['quizzes_completed'] == 1
       badge = Badge.get(:name => 'Completed 1 Quiz')
-    elsif :quizzes_completed == 5
+    elsif self['quizzes_completed'] == 5
       badge = Badge.get(:name => 'Completed 5 Quizzes')
-    elsif :quizzes_completed == 10
+    elsif self['quizzes_completed'] == 10
       badge = Badge.get(:name => 'Completed 10 Quizzes')
     end
     if badge
-      :badges << badge
+      self.badges << badge
+      self.save
     end
   end
 
   ## Increments the number of comments left, and checks for earned badges.
   def increment_comments
-    self.update(:comments_left => (:comments_left + 1))
-    if :comments_left == 1
+    new_comments_left = (self['comments_left'] + 1)
+    self.update(:comments_left => new_comments_left)
+    if self['comments_left'] == 1
       badge = Badge.get(:name => 'Left 1 Comment')
-    elsif :comments_left == 5
+    elsif self['comments_left'] == 5
       badge = Badge.get(:name => 'Left 5 Comments')
-    elsif :comments_left == 10
+    elsif self['comments_left'] == 10
       badge = Badge.get(:name => 'Left 10 Comments')
     end
     if badge
-      :badges << badge
+      BadgeUser.create(:user => self, :badge => badge)
     end
   end
 
   def earn_xp(xp_earned)
-    self.update(:xp => (:xp + xp_earned))
+    new_xp = (self['xp'] + xp_earned)
+    self.update(:xp => new_xp)
     self.update_level
   end
 end

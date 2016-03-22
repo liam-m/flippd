df33af600cb99cc95ad8fb0d5991c7c5d4e8b989
Kay Sharrock <sharrock.kay@gmail.com>
2016-03-22 13:30:09 +0000

    Prevent badges being duplicated.

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -7,33 +7,35 @@ class Badge
   property :requirement, String, required: true, length: 150
   property :required_value, Integer, required: true
 
-  belongs to, :users, :through => Resource
+  has n, :users, :through => Resource
 
   ## This would ideally be replaced with a JSON import.
   def Badge.populate
-    Badge.create(:name => 'Completed 1 Quiz',
-                 :image => "badge.png",
-                 :requirement => "quizzes_complete",
-                 :required_value => 1)
-    Badge.create(:name => "Completed 5 Quizzes",
-                 :image => "badge.png",
-                 :requirement => "quizzes_complete",
-                 :required_value => 5)
-    Badge.create(:name => "Completed 10 Quizzes",
-                 :image => "badge.png",
-                 :requirement => "quizzes_complete",
-                 :required_value => 10)
-    Badge.create(:name => "Left 1 Comment",
-                 :image => "badge.png",
-                 :requirement => "comments_left",
-                 :required_value => 1)
-    Badge.create(:name => "Left 5 Comments",
-                 :image => "badge.png",
-                 :requirement => "comments_left",
-                 :required_value => 5)
-    Badge.create(:name => "Left 10 Comments",
-                 :image => "badge.png",
-                 :requirement => "comments_left",
-                 :required_value => 10)
+    if Badge.count == 0
+      Badge.create(:name => 'Completed 1 Quiz',
+                   :image => "badge.png",
+                   :requirement => "quizzes_complete",
+                   :required_value => 1)
+      Badge.create(:name => "Completed 5 Quizzes",
+                   :image => "badge.png",
+                   :requirement => "quizzes_complete",
+                   :required_value => 5)
+      Badge.create(:name => "Completed 10 Quizzes",
+                   :image => "badge.png",
+                   :requirement => "quizzes_complete",
+                   :required_value => 10)
+      Badge.create(:name => "Left 1 Comment",
+                   :image => "badge.png",
+                   :requirement => "comments_left",
+                   :required_value => 1)
+      Badge.create(:name => "Left 5 Comments",
+                   :image => "badge.png",
+                   :requirement => "comments_left",
+                   :required_value => 5)
+      Badge.create(:name => "Left 10 Comments",
+                   :image => "badge.png",
+                   :requirement => "comments_left",
+                   :required_value => 10)
+    end
   end
 end
